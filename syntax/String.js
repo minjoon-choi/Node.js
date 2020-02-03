3e874bc1486604d83a6eb217903c2c15bd563a05
minjoon_choi <mjchois@gmail.com>
2020-02-03 23:11:45 +0900

    Node.js study

---
+++ syntax/String.js
@@ -0,0 +1,5 @@
+// string + string
+console.log('1'+'1');
+
+// JavaScript String length Property
+console.log('Lorem ipsum dolor sit amet, cocnsectetur adipisicing'.length);
---
+++ syntax/Variable.js
@@ -0,0 +1,6 @@
+var a = 1;
+console.log(a);
+// 변수 선언 시 var은 1개 변수에 대해 최초 한 번만 하면 됨 
+a = 2;
+console.log(a);
+//1 = 2; comment
